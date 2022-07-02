b171b97dedc727e01d2e20edd491182d2653b486
AshrafulIslamAlif <alif.ashrafulislam@gmail.com>
2022-07-02 09:24:07 +0600

    mongo commenct

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -14,7 +14,44 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log("database connected");
+        const taskManagementCollection = client.db('task_management').collection('daily_task');
+
+        //add task to db
+        app.post('/user', async (req, res) => {
+            const newTask = req.body;
+            console.log(newTask);
+            const result = await taskManagementCollection.insertOne(newTask)
+            res.send('post method success')
+        })
+
+        //read all task
+        app.get('/user', async (req, res) => {
+            const query = {};
+            const cursor = taskManagementCollection.find(query);
+            const tasks = await cursor.toArray();
+            res.send(tasks);
+        });
+
+        //read task by id
+        app.get('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await taskManagementCollection.findOne(query);
+            res.send(result);
+        })
+
+        //update user
+        app.put('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const editTask = req.body;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: editTask
+            }
+            const result = await taskManagementCollection.updateOne(filter, updateDoc, options)
+            res.send(result);
+        })
     }
     finally {
 
